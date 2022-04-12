93dcfb14d452106cf988262f5ab8bcc4e3213401
Md Monirul Islam <monirul@Mds-MacBook-Pro.local>
2022-04-12 18:47:14 -0400

    initial

---
+++ src/lottery.js
@@ -0,0 +1,7 @@
+import web3 from "./web3";
+
+// exports the copy of the contract
+const address = '0xC4361A7784B384B3D18eF59b0046cbCDe84d9ceb'
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
+
+export default new web3.eth.Contract(abi, address)
